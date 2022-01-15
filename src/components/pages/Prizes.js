8e370b0d71f2b11dbb7c41f921524332b8899f7e
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-01-16 02:59:38 +0530

    prize page

--- src/components/pages/Prizes.js
+++ src/components/pages/Prizes.js
@@ -9,20 +9,23 @@ const Container = styled.div`
   justify-content: center;
 `;
 const Heading = styled.h1`
-  background-color: blue;
   height: 7vh;
   width: 80%;
   padding: 0;
   margin: 0 auto;
+  text-align: center;
+  color: white;
+  font-family: montserrat;
+  font-size: 2rem;
 `;
 const ContainerOne = styled.div`
   height: 100vh;
   width: 100%;
   background-color: red;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  justify-content:space-around;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 const ContainerTwo = styled.div`
   height: 100vh;
@@ -43,39 +46,100 @@ const Helper = styled.div`
   justify-content: space-between;
 `;
 const InsideHelper = styled.div`
-  height: 50vh;
+  height: 30vh;
   width: 80%;
   display: flex;
-  background-color: blue;
-  margin:0 auto;
+  margin: 0 auto;
 `;
 const Strip = styled.div`
-  height: 10vh;
-  width: 80%;
+  height: 60vh;
+  width: 100%;
   margin: 0 auto;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const StripOne = styled.div`
+  height: 2rem;
+  background-color: #6809ac;
+  width: 100%;
+  margin-bottom: 1vh;
+`;
+const StripTwo = styled.div`
+  height: 30vh;
+  background-color: #6809ac;
+  width: 100%;
+  margin-bottom: 1vh;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 const PrizeOne = styled.div`
-background-color: pink;
-// margin:2px;
-width:50%;
+  width: 50%;
+  position:relative
 `;
 const PrizeTwo = styled.div`
-background-color: pink;
-// margin:2px;
-width:50%;
+  width: 50%;
+  position:relative
+`;
+const Header = styled.h1`
+font-size:3rem;
+color:white;
+position:absolute;
+top: ${props => props.top};
+right: ${props => props.right};
+`;
+const Prize = styled.h2`
+font-size:3rem;
+color: #A979EF;
+position:absolute;
+top: ${props => props.top};
+right: ${props => props.right};
+font-weight:900;
+font-family:montserrat;
+`;
+const DivOne = styled.div`
+  color: white;
+  font-size: 2.5rem;
+  font-family: montserrat;
+  margin-bottom: 2vh;
+`;
+const Hr = styled.hr`
+  color: white;
+  width: 10rem;
+  margin-bottom: 2vh;
+`;
+const DivThree = styled.div`
+  color: white;
+  font-size: 2rem;
+  font-family: montserrat;
+  margin-bottom: 2vh;
 `;
 export default function Prizes() {
   return (
     <Container>
       <ContainerOne>
-        <Heading>Prizes</Heading>
-        <Strip>prize pool</Strip>
+        <Heading>PRIZES</Heading>
+        <Strip>
+          <StripOne></StripOne>
+          <StripTwo>
+            <DivOne>PRIZE POOL</DivOne>
+            <Hr />
+            <DivThree>₹ 55000 +</DivThree>
+          </StripTwo>
+          <StripOne></StripOne>
+        </Strip>
         <InsideHelper>
-          <PrizeOne>first prize</PrizeOne>
-          <PrizeTwo>second prize</PrizeTwo>
+          <PrizeOne>
+            <Header top={"0"}>FIRST PRIZE</Header>
+            <Prize top={"60px"}>₹ 7000</Prize>
+          </PrizeOne>
+          <PrizeTwo>
+            <Header top={"300px"} right={"0"}>SECOND PRIZE</Header>
+            <Prize top={"360px"} right={"0"}>₹ 7000</Prize>
+          </PrizeTwo>
         </InsideHelper>
       </ContainerOne>
       <ContainerTwo>
