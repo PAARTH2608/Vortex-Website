e32358868b0e6b1a789d78c749f2687e3be3ed35
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-01-18 01:08:24 +0530

    fix: scrolling

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import scrollSlice from './scroll-slice';
-
-const store = configureStore({
-    reducer: { scroll:scrollSlice.reducer }
-}); 
-export default store; 
